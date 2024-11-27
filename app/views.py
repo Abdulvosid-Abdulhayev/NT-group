17c6a06f2bab63899fad24affd307443d986ee85
Abudlvosid Abdulhayev <abdulvosid780@gmail.com>
2024-11-27 15:49:08 +0500

    permission_change

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from .serializers import *
 from rest_framework import filters
 # Create your views here.
 from rest_framework import generics
+from rest_framework import permissions
 
 class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -11,21 +12,25 @@ class UserView(generics.ListCreateAPIView):
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['name', 'email', 'created_at']
     search_fields = ['name', 'email', 'created_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-    
+
 class CourseView(generics.ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['name', 'created_at']
     search_fields = ['name', 'created_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     
 class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     
