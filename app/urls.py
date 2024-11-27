62d3effaaf96777e0ab4a2348aa17c36393766bf
Abudlvosid Abdulhayev <abdulvosid780@gmail.com>
2024-11-27 15:32:09 +0500

    first_change

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,6 @@ from .views import *
 urlpatterns = [
     path('user/', UserView.as_view(), name = "User"),
     path('user/<int:pk>/', UserDetailView.as_view(), name = "User_detail"),
+    path("course/", CourseView.as_view(), name = "Course"),
+    path("course/<int:pk>", CourseDetailView.as_view(), name = "Course_detail"),
 ]
--- app/views.py
+++ app/views.py
@@ -15,5 +15,17 @@ class UserView(generics.ListCreateAPIView):
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['name', 'email', 'created_at']
+
+    
+class CourseView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['name', 'created_at']
+    search_fields = ['name', 'created_at']
+    
+class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    
